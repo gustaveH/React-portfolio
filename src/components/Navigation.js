1a040486b624cf38d0d061abe1ed66e31bff4466
gustaveH <ghabiman@gmail.com>
2020-07-23 13:21:41 -0400

    css changes to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,37 @@ import React from 'react';
 class Navigation extends React.Component {
   render() {
     return (
-      <div className='ui menu navbar'>
-        <a className='active left floated item' href='/#'>
-          About me
-        </a>
-        <a className='item' href='/#'>
-          Skills
-        </a>
-        <a className='item' href='/#'>
-          Portfolio
-        </a>
-        <a className='item' href='/#'>
-          Contact
-        </a>
-        <a className='item' href='/#'>
-          Writing
-        </a>
+      <div className='ui menu'>
+        <div className='logo' href='/#'>
+          <h4>Gustave</h4>
+        </div>
+        <ul className='nav-links'>
+          <li>
+            <a className='item' href='/#'>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className='item' href='/#'>
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className='item' href='/#'>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a className='item' href='/#'>
+              Writing
+            </a>
+          </li>
+        </ul>
+        <div className='burger'>
+          <div className='line1'></div>
+          <div className='line2'></div>
+          <div className='line3'></div>
+        </div>
       </div>
     );
   }
